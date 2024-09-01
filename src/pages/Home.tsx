2e96391e82963035b6cb6ce3675e67f476a52251
Ana Clara <ana4ew.br@gmail.com>
2024-09-01 19:25:14 -0300

    Created: New hero style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,37 @@
 import { BrowserRouter } from 'react-router-dom';
+
 import Header from '../components/Header';
+import HeroRectangleOne from "../assets/images/rectangleOne.svg";
+import HeroRectangleTwo from "../assets/images/rectangleTwo.svg";
+
+import Button from "../components/Button";
+import "../styles/hero.css";
+
 
 export default function Home() {
     return (
         <BrowserRouter>
             <Header />
+            <section id="hero">
+                <span className="desktop-only">
+                    <img src={HeroRectangleTwo} alt="Retangulo um tela inicial" />
+                </span>
+                <img src={HeroRectangleOne} alt="Retangulo dois tela inicial" />
+                <div className="container content">
+                <p className="desktop-only">
+                    Olá
+                </p>
+                <h1>Comida de mãe direto no seu apê, é só pedir que entregamos para você!</h1>
+                <p>Já pensou em matar a saudade daquela comida caseira? O melhor de tudo, nossas receitas são 100% saudáveis, bora entrar no shape.
+                </p>
+                <div className="flex gap-1">
+                    <span><Button text="Cadastre-se" /></span>
+                    <span className="desktop-only">
+                        <Button text="Veja mais" secondary />
+                    </span>
+                </div>
+            </div>
+            </section>
         </BrowserRouter>
     )
 }
