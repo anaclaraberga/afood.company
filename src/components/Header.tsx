945c6ec07388d8826eb5bbe2048d14e93560f2c6
Ana Clara <ana4ew.br@gmail.com>
2024-09-02 20:56:38 -0300

    Fixed: Menu bar without background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,77 +8,78 @@ import Close from "../assets/icons/close.svg"
 
 export default function Home() {
     const [showMobileMenu, setShowMobileMenu] = useState(false);
-    
+
     return (
         <>
             <header className="container py-sm">
-            <nav className="flex items-center justify-between">
-                <img src={Logo} alt="Logo" width={105} height={40} />
+                <nav className="flex items-center justify-between">
+                    <img src={Logo} alt="Logo" width={105} height={40} />
 
-                <div className="desktop-only">
-                <ul className="flex gap-1">
-                    <li>
-                        <a href="#">Home</a>
-                    </li>
-                    <li>
-                        <a href="#solution">Soluções</a>
-                    </li>
-                    <li>
-                        <a href="#testimonials">Depoimentos</a>
-                    </li>
-                    <li>
-                        <a href="#pricing">Preços</a>
-                    </li>
-                    <li>
-                        <a href="#contact">Contato</a>
-                    </li>
-                </ul>
-            </div>
+                    <div className="desktop-only">
+                        <ul className="flex gap-1">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#solution">Soluções</a>
+                            </li>
+                            <li>
+                                <a href="#testimonials">Depoimentos</a>
+                            </li>
+                            <li>
+                                <a href="#pricing">Preços</a>
+                            </li>
+                            <li>
+                                <a href="#contact">Contato</a>
+                            </li>
+                        </ul>
+                    </div>
 
-            <div className="desktop-only">
-                <div className="flex items-center">
-                    <a className="reverse-color ml-lg" href="">Login</a>
-                    <Button text="Cadastre-se" />
-                </div>
-            </div>
-            </nav>
-
-            <div className="mobile-menu">
-                {showMobileMenu ?
-                    <div className="mobile-menu-content">
-                        <div className="container flex">
-                            <ul>
-                                <li>
-                                    <a href="#">Home</a>
-                                </li>
-                                <li>
-                                    <a href="#solution">Soluções</a>
-                                </li>
-                                <li>
-                                    <a href="#testimonials">Depoimentos</a>
-                                </li>
-                                <li>
-                                    <a href="#pricing">Preços</a>
-                                </li>
-                                <li>
-                                    <a href="#contact">Contato</a>
-                                </li>
-                                <li>
-                                    <a className="reverse-color" href="#">Login</a>
-                                </li>
-                            </ul>
-                            <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
-                                <img src={Close} alt="ícone fechar menu" width={24} height={24} />
-                            </span>
+                    <div className="desktop-only">
+                        <div className="flex items-center">
+                            <a className="reverse-color ml-lg" href="">Login</a>
+                            <Button text="Cadastre-se" />
                         </div>
                     </div>
-                    :
-                    <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
-                        <img src={Menu} alt="ícone menu" width={24} height={24} />
-                    </span>
-                }
-            </div>
-            
+
+                    <div className="mobile-menu">
+                    {showMobileMenu ?
+                        <div className="mobile-menu-content">
+                            <div className="container flex">
+                                <ul>
+                                    <li>
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li>
+                                        <a href="#solution">Soluções</a>
+                                    </li>
+                                    <li>
+                                        <a href="#testimonials">Depoimentos</a>
+                                    </li>
+                                    <li>
+                                        <a href="#pricing">Preços</a>
+                                    </li>
+                                    <li>
+                                        <a href="#contact">Contato</a>
+                                    </li>
+                                    <li>
+                                        <a className="reverse-color" href="#">Login</a>
+                                    </li>
+                                </ul>
+                                <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper">
+                                    <img src={Close} alt="ícone fechar menu" width={24} height={24} />
+                                </span>
+                            </div>
+                        </div>
+                        :
+                        <span onClick={() => setShowMobileMenu(!showMobileMenu)} className="btn-wrapper" >
+                            <img src={Menu} alt="ícone menu" width={24} height={24} />
+                        </span>
+                    }
+                </div>
+                
+                </nav>
+
             </header>
         </>
     )
